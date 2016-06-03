1dd2eba53fd5c2ad6f4d7df510f1584c19bff729
Simon Krajewski <simon@haxe.org>
2016-06-03 16:38:16 +0200

    fix endianess

--- javalib/jWriter.ml
+++ javalib/jWriter.ml
@@ -17,8 +17,8 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  *)
 open JData;;
-open IO.BigEndian;;
 open IO;;
+open IO.BigEndian;;
 open ExtString;;
 open ExtList;;
 
