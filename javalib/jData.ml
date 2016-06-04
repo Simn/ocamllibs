3b76f3e8044b953c88a3fd5748f1651f1c3e31c9
Simon Krajewski <simon@haxe.org>
2016-06-04 15:16:35 +0200

    add opcode printing

--- javalib/jData.ml
+++ javalib/jData.ml
@@ -440,3 +440,172 @@ let s_field f = (if is_override f then "override " else "") ^ s_sig f.jf_signatu
 
 let s_fields fs = "{ \n\t" ^ String.concat "\n\t" (List.map s_field fs) ^ "\n}"
 
+let s_jcode code =
+  let wi s i =
+    Printf.sprintf "%s %i" s i
+  in
+  let wp s path =
+    Printf.sprintf "%s %s" s (snd path)
+  in
+  match code with
+  (* double *)
+  | OpD2f -> "d2f"
+  | OpD2i -> "d2i"
+  | OpD2l -> "d2l"
+  | OpDadd -> "dadd"
+  | OpDaload -> "daload"
+  | OpDastore -> "dastore"
+  | OpDcmpg -> "dcmpg"
+  | OpDcmpl -> "dcmpl"
+  | OpDdiv -> "ddiv"
+  | OpDconst_0 -> "dconst_0"
+  | OpDconst_1 -> "dconst_1"
+  | OpDload i -> wi "dload" i
+  | OpDmul -> "dmul"
+  | OpDneg -> "dneg"
+  | OpDrem -> "drem"
+  | OpDreturn -> "dreturn"
+  | OpDstore i -> wi "dstore" i
+  | OpDsub -> "dsub"
+  (* float *)
+  | OpF2d -> "f2d"
+  | OpF2i -> "f2i"
+  | OpF2l -> "f2l"
+  | OpFadd -> "fadd"
+  | OpFaload -> "faload"
+  | OpFastore -> "fastore"
+  | OpFcmpg -> "fcmpg"
+  | OpFcmpl -> "fcmpl"
+  | OpFdiv -> "fdiv"
+  | OpFconst_0 -> "fconst_0"
+  | OpFconst_1 -> "fconst_1"
+  | OpFconst_2 -> "fconst_2"
+  | OpFload i -> wi "fload" i
+  | OpFmul -> "fmul"
+  | OpFneg -> "fneg"
+  | OpFrem -> "frem"
+  | OpFreturn -> "freturn"
+  | OpFstore i -> wi "fstore" i
+  | OPFsub -> "fsub"
+  (* int *)
+  | OpI2b -> "i2b"
+  | OpI2c -> "i2c"
+  | OpI2d -> "i2d"
+  | OpI2f -> "i2f"
+  | OpI2l -> "i2l"
+  | OpI2s -> "i2s"
+  | OpIadd -> "iadd"
+  | OpIaload -> "iaload"
+  | OpIand -> "iand"
+  | OpIastore -> "iastore"
+  | OpIconst_m1 -> "iconst_m1"
+  | OpIconst_0 -> "iconst_0"
+  | OpIconst_1 -> "iconst_1"
+  | OpIconst_2 -> "iconst_2"
+  | OpIconst_3 -> "iconst_3"
+  | OpIconst_4 -> "iconst_4"
+  | OpIconst_5 -> "iconst_5"
+  | OpIdiv -> "idiv"
+  | OpIload i -> wi "iload" i
+  | OpImul -> "imul"
+  | OpIneg -> "ineg"
+  | OpIor -> "ior"
+  | OpIrem -> "irem"
+  | OpIreturn -> "ireturn"
+  | OpIshl -> "ishl"
+  | OpIshr -> "ishr"
+  | OpIstore i -> wi "istore" i
+  | OpIsub -> "isub"
+  | OpIushr -> "iushr"
+  | OpIxor -> "ixor"
+  (* long *)
+  | OpL2d -> "l2d"
+  | OpL2f -> "l2f"
+  | OpL2i -> "l2i"
+  | OpLadd -> "ladd"
+  | OpLaload -> "laload"
+  | OpLand -> "land"
+  | OpLastore -> "lastore"
+  | OpLcmp -> "lcmp"
+  | OpLdiv -> "ldiv"
+  | OpLload i -> wi "lload" i
+  | OpLmul -> "lmul"
+  | OpLneg -> "lneg"
+  | OpLor -> "lor"
+  | OpLrem -> "lrem"
+  | OpLreturn -> "lreturn"
+  | OpLshl -> "lshl"
+  | OpLshr -> "lshr"
+  | OpLstore i -> wi "lstore" i
+  | OpLsub -> "lsub"
+  | OpLushr -> "lushr"
+  | OpLxor -> "lxor"
+  (* short *)
+  | OpSaload -> "saload"
+  | OpSastore -> "sastore"
+  | OpSipush i -> wi "sipush" i
+  (* array *)
+  | OpAaload -> "aaload"
+  | OpAastore -> "aastore"
+  | OpAnewarray path -> wp "anewarray" path
+  | OpArraylength -> "arraylength"
+  | OpBaload -> "baload"
+  | OpBastore -> "bastore"
+  | OpBipush i -> wi "bipush" i
+  | OpCaload -> "caload"
+  | OpCastore -> "castore"
+  | OpMultianewarray(path,i) -> "multinewarray" (* TODO *)
+  | OpNewarray(jsig) -> "newarray" (* TODO *)
+  (* reference *)
+  | OpAload i -> wi "aload" i
+  | OpAreturn -> "areturn"
+  | OpAstore i -> wi "astore" i
+  (* object *)
+  | OpNew path -> wp "new" path
+  | Opinstanceof path -> wp "instanceof" path
+  | OpCheckcast path -> wp "checkcast" path
+  | OpInvokedynamic arg -> "invokedynamic"
+  | OpInvokeinterface(arg1,arg2) -> "invokeinterface"
+  | OpInvokespecial arg1 -> "invokespecial"
+  | OpInvokestatic arg1 -> "invokestatic"
+  | OpInvokevirtual arg1 -> "invokevirtual"
+  | OpGetfield arg1 -> "getfield"
+  | OpGetstatic arg1 -> "getstatic"
+  | OpPutfield arg1 -> "putfield"
+  | OpPutstatic arg1 -> "putstatic"
+  (* branching *)
+  | OpIf_acmpeq i -> wi "acmpeq" i
+  | OpIf_acmpne i -> wi "acmpne" i
+  | OpIf_icmp(cmp,i) -> wi "icmp" i (* TODO *)
+  | OpIf(cmp,i) -> wi "if" i (* TODO *)
+  | OpIfnonnull i -> wi "ifnotnull" i
+  | OpIfnull i -> wi "ifnull" i
+  | OpGoto i -> wi "goto" i
+  | OpGoto_w i -> wi "goto_w" i
+  | OpJsr i -> wi "jsr" i
+  | OpJsr_w i -> wi "jsr_w" i
+  (* stack *)
+  | OpAconst_null -> "aconst_null"
+  | OpDup -> "dup"
+  | OpDup_x1 -> "dup_x1"
+  | OpDup_x2 -> "dup_x2"
+  | OpDup2 -> "dup2"
+  | OpDup2_x1 -> "dup2_x1"
+  | OpDup2_x2 -> "dup2_x2"
+  | OpLdc i -> wi "ldc" i
+  | OpLdc_w i -> wi "ldc_w" i
+  | OpLdc2_w i -> wi "ldc2_w" i
+  | OpNop -> "nop"
+  | OpPop -> "pop"
+  | OpPop2 -> "pop2"
+  | OpSwap -> "swap"
+  (* other *)
+  | OpAthrow -> "athrow"
+  | OpIinc(i1,i2) -> wi "iinc" i1 (* TODO *)
+  | OpLookupswitch -> "lookupswitch"
+  | OpMonitorenter -> "monitorenter"
+  | OpMonitorexit -> "monitorexit"
+  | OpRet i -> wi "ret" i
+  | OpReturn -> "return"
+  | OpTableswitch -> "tableswitch"
+  | OpWide -> "wide"
